5ae8f6da8fa32f31756d803394833f88dfffa3a0
kai <yash.amberkar005@gmail.com>
2024-10-18 15:36:30 +0530

    rebuild

--- app/(pages)/user/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function User() {
-	return (
-		<div className='font-Satoshi_B text-3xl w-full h-screen flex items-center justify-center'>
-			DOWMLOAD THE MOBILE APP FOR MORE INFO
-		</div>
-	);
-}
--- lib/lucia/adapter.ts
+++
@@ -1,6 +0,0 @@
-import db from "@/lib/db";
-import { sessionTable, userTable } from "@/lib/models/schema";
-import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
-
-const adapter = new DrizzlePostgreSQLAdapter(db, sessionTable, userTable);
-export default adapter;
